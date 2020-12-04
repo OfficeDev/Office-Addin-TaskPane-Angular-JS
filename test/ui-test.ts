f17c1a284c9c9024964d75684125db3d0faee6ca
Courtney Owen <cowen@microsoft.com>
2020-12-04 11:41:43 -0800

    Disable debugging when calling startDebugging for UI tests
    - We need to do this for our tests since we added a new debug dialog, which will be open when direct debugging is enabled for the add-in and this will disrupt the flow of our tests

--- test/ui-test.ts
+++ test/ui-test.ts
@@ -2,7 +2,6 @@ import * as assert from "assert";
 import * as mocha from "mocha";
 import { parseNumber } from "office-addin-cli";
 import { AppType, startDebugging, stopDebugging } from "office-addin-debugging";
-import { DebuggingMethod } from "office-addin-dev-settings";
 import { toOfficeApp } from "office-addin-manifest";
 import * as officeAddinTestHelpers from "office-addin-test-helpers";
 import * as officeAddinTestServer from "office-addin-test-server";
@@ -28,7 +27,8 @@ hosts.forEach(function (host) {
             // Call startDebugging to start dev-server and sideload
             const devServerCmd = `npm run dev-server -- --config ./test/webpack.config.js`;
             const devServerPort = parseNumber(process.env.npm_package_config_dev_server_port || 3000);
-            await startDebugging(manifestPath, AppType.Desktop, toOfficeApp(host), DebuggingMethod.Proxy, undefined, devServerCmd, devServerPort);
+            await startDebugging(manifestPath, AppType.Desktop, toOfficeApp(host), undefined,
+                undefined, devServerCmd, devServerPort, undefined, undefined, undefined, false /* enableDebugging */);
         }),
         describe(`Get test results for ${host} taskpane project`, function () {
             it("Validate expected result count", async function () {
